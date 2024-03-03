9c3c31d76da5e6c867a04afd1fdf9ffa5b280a1b
Sylvain Marty <10723351+SylvainMarty@users.noreply.github.com>
2024-03-03 13:27:04 +0100

    chore: prepare file structure & modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from './config/config.module';
+import { CompaniesModule } from './companies/companies.module';
+import { CensusesModule } from './censuses/censuses.module';
+import { ApiModule } from './api/api.module';
 
 @Module({
-  imports: [ConfigModule],
+  imports: [ConfigModule, CompaniesModule, CensusesModule, ApiModule],
 })
 export class AppModule {}
