57a11fbb80f6dac3f022a37768ec32e096b56a87
Sylvain Marty <10723351+SylvainMarty@users.noreply.github.com>
2024-03-05 23:37:07 +0100

    fix: remove out-of-scope censuses module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from './config/config.module';
 import { CompaniesModule } from './companies/companies.module';
-import { CensusesModule } from './censuses/censuses.module';
 import { ApiModule } from './api/api.module';
 
 @Module({
-  imports: [ConfigModule, CompaniesModule, CensusesModule, ApiModule],
+  imports: [ConfigModule, CompaniesModule, ApiModule],
 })
 export class AppModule {}
